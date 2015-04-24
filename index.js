a69df8ce21bc51bd0dcbe692e8815660e17e3930
Scott Fayer <scott.fayer@gmail.com>
2015-04-23 19:44:58 -0600

    4/23/15 - commiting last weeks notes. sfayer

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 'use strict';
+
+// Library starts out in the 'lib' folder
+// -- Automatically loads index.js file
+// -- if we wanted person.js, syntax = './lib/person'
+
+//module.exports = require('./lib');
+
+var lib = require('./lib');
+lib.getAge("Scott");
+
